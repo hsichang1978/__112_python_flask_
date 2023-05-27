4db76a5e59397f22c1ed5d58d4c88be5b38ac492
hsichang1978 <126947012+hsichang1978@users.noreply.github.com>
2023-05-27 06:21:59 +0000

    modify

--- lesson8_1.py
+++ lesson8_1.py
@@ -1,11 +1,21 @@
+
 import requests
+import pandas as pd
 url = 'https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json'
 response = requests.request('GET',url)
 if response.status_code == 200:
     print("連線成功")
-    print(response.text)    #text是屬性，不要用括號
-#【json檔】是java的物件，為純文字複雜資料，有可能為list內，包含了dictionary。
-#承上，dictionary內的value又包含了list，故稱純文字複雜資料。
+    all_data = response.json()
+    print(type(all_data))
 else:
-    print(f"連線失敗{response.status_cpde}")
+    print(f"連線失敗:{response.status_code}")
 
+
+
+dataFrame = pd.DataFrame(data=all_data,columns=['sna','tot','sbi','sarea','mday','ar','bemp','act'])
+min = int(input("請輸入要查詢的可借數量:"))
+mask = dataFrame['sbi'] <= min
+mask_dataFrame = dataFrame[mask]
+mask_dataFrame.to_csv('可借小於3的站點.csv')
+filename = f'可借小於{min}的站點.xlsx'
+mask_dataFrame.to_excel(filename)
